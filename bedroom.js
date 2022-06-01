b3484383e161b29fa7f5eb23c4eb56a52b1f2ced
Hasika456 <85883194+Hasika456@users.noreply.github.com>
2022-06-01 17:43:03 +0530

    Add files via upload

---
+++ bedroom.js
@@ -0,0 +1,52 @@
+var status="";
+
+img="";
+
+objects="";
+
+function preload(){
+    img=loadImage('bedroom.png');
+}
+
+function back_btn(){
+    window.location="index.html";
+}
+
+function setup(){
+    canvas = createCanvas(640, 420);
+    canvas.center();
+
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML="Status : Detecting Objects";
+    
+}
+
+function draw(){
+    image(img, 0, 0, 640, 420);
+    if(status != ""){
+        for(i=0; i<objects.length; i++){
+            document.getElementById("status").innerHTML="Status : Objects detected";
+            fill("#eb8f52");
+            percent=floor(objects[i].confidence * 100);
+            text(objects[i].label+ "   " + percent + "%", objects[i].x+15, objects[i].y-15);
+            noFill();
+            stroke("#eb8f52");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+
+}
+
+function modelLoaded(){
+    console.log("Model loaded!");
+    status=true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+    objects=results;
+}
---
+++ main.js
@@ -0,0 +1,15 @@
+function bedroom(){
+    window.location="bedroom.html";
+}
+
+function tv(){
+    window.location="tv.html";
+}
+
+function bottles(){
+    window.location="waterbottles.html";
+}
+
+function desk(){
+    window.location="desk.html";
+}
